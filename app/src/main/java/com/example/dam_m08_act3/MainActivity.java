63231b46bec0ef007dddaa9db106e2e56b0eec2b
uriigrao <oriolgm375@gmail.com>
2020-11-15 13:54:41 +0100

    añadidas funciones de play stop pause

--- app/src/main/java/com/example/dam_m08_act3/MainActivity.java
+++ app/src/main/java/com/example/dam_m08_act3/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.dam_m08_act3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pause(View v) {
+        if (mp != null && mp.isPlaying()) {
+            position = mp.getCurrentPosition();
+            mp.pause();
+        }
+    }
+
+    public void play(View v) {
+        if (mp != null && !mp.isPlaying()) {
+            mp.seekTo(position);
+            mp.start();
+        } else {
+            mp.release();
+            // mp = MediaPlayer.create(this, R.raw.numeros); todo: falta revisar.
+        }
+    }
+
+    public void stop(View v) {
+        if (mp != null) {
+            mp.stop();
+            position = 0;
+        }
     }
 }
