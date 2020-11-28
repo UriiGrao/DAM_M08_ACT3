ba15036562b40c5b225010017fb221bf97992f2a
uriigrao <oriolgm375@gmail.com>
2020-11-28 13:14:35 +0100

    removed permissions for take videos and put in functions lambda

--- app/src/main/java/com/example/dam_m08_act3/MainActivity.java
+++ app/src/main/java/com/example/dam_m08_act3/MainActivity.java
@@ -18,25 +18,19 @@ import android.widget.VideoView;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-
 
     VideoView videoView;
-
+    Button buttonPause;
+    Button buttonPlay;
+    Button buttonStop;
+    Button buttonSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS_STORAGE,
-                REQUEST_EXTERNAL_STORAGE);
-
-        videoView = (VideoView) findViewById(R.id.videoViewScreen);
+        videoView = findViewById(R.id.videoViewScreen);
 
         String nameVideo = getIntent().getStringExtra("videoName");
 
@@ -47,39 +41,23 @@ public class MainActivity extends AppCompatActivity {
         }
         videoView.start();
 
-        Button buttonPause = (Button) findViewById(R.id.buttonpause);
-        Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
-        Button buttonStop = (Button) findViewById(R.id.buttonStop);
-        Button buttonSelect = (Button) findViewById(R.id.buttonSelectVideo);
+        buttonPause = findViewById(R.id.buttonpause);
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonSelect = findViewById(R.id.buttonSelectVideo);
 
-        buttonSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent nextPage = new Intent(MainActivity.this, VideoDirectory.class);
-                startActivity(nextPage);
-            }
+        buttonSelect.setOnClickListener(v -> {
+            Intent nextPage = new Intent(MainActivity.this, VideoDirectory.class);
+            startActivity(nextPage);
         });
 
-        buttonPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                videoView.pause();
-            }
-        });
+        buttonPause.setOnClickListener(v -> videoView.pause());
 
-        buttonPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                videoView.start();
-            }
-        });
+        buttonPlay.setOnClickListener(v -> videoView.start());
 
-        buttonStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                videoView.seekTo(0);
-                videoView.pause();
-            }
+        buttonStop.setOnClickListener(v -> {
+            videoView.seekTo(0);
+            videoView.pause();
         });
     }
 
