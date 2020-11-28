aea1e3b9aebe4c5b2607a202bad65ae8aead7560
uriigrao <oriolgm375@gmail.com>
2020-11-28 13:21:36 +0100

    the same from MainActivity to VideoDirectory

--- app/src/main/java/com/example/dam_m08_act3/VideoDirectory.java
+++ app/src/main/java/com/example/dam_m08_act3/VideoDirectory.java
@@ -21,41 +21,28 @@ import java.util.ArrayList;
 import java.util.ListIterator;
 
 public class VideoDirectory extends AppCompatActivity {
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_directory);
 
-        ActivityCompat.requestPermissions(VideoDirectory.this, PERMISSIONS_STORAGE,
-                REQUEST_EXTERNAL_STORAGE);
 
-        ListView list=(ListView) findViewById( R.id.listVideos);
-        ArrayAdapter<String> nameVideo;
+        ListView list = findViewById(R.id.listVideos);
         ArrayList<String> arrayList = new ArrayList<>();
-        // Captar ficheros de un directorio en concreto
-        //getClass().getResource("raw/noragami.mp4").toURI();
+
         Field[] fields = R.raw.class.getFields();
-        for (Field field:
-             fields) {
+        for (Field field : fields)
             arrayList.add(field.getName());
-        }
-        list.setAdapter(new ArrayAdapter<String>(VideoDirectory.this,
+        list.setAdapter(new ArrayAdapter<>(VideoDirectory.this,
                 android.R.layout.simple_list_item_1, arrayList));
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selected = list.getAdapter().getItem(position).toString();
-                Intent nextPage = new Intent(VideoDirectory.this, MainActivity.class);
-                nextPage.putExtra("videoName", selected);
-                startActivity(nextPage);
-            }
+        list.setOnItemClickListener((parent, view, position, id) -> {
+            String selected = list.getAdapter().getItem(position).toString();
+            Intent nextPage = new Intent(VideoDirectory.this, MainActivity.class);
+            nextPage.putExtra("videoName", selected);
+            startActivity(nextPage);
         });
     }
 }
